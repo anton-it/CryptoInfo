92f4df5122b5d5e12ed6c5e90ead47fb54da0d5f
Admin1 <a89086829981@gmail.com>
2023-02-14 12:57:58 +0300

    Add adapter

--- app/src/main/java/com/ak87/cryptoinfo/CoinPriceListActivity.kt
+++ app/src/main/java/com/ak87/cryptoinfo/CoinPriceListActivity.kt
@@ -1,30 +1,40 @@
 package com.ak87.cryptoinfo
 
+import android.content.ClipData
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.ak87.cryptoinfo.adapters.CoinInfoAdapter
+import com.ak87.cryptoinfo.databinding.ActivityCoinPriceListBinding
+import com.ak87.cryptoinfo.databinding.ItemCoinInfoBinding
+import com.ak87.cryptoinfo.pojo.CoinPriceInfo
 
 
 class CoinPriceListActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityCoinPriceListBinding
     private lateinit var viewModel: CoinViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_coin_price_list)
+        binding = ActivityCoinPriceListBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        val adapter = CoinInfoAdapter(this)
+        adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
+            override fun onClick(coinPriceInfo: CoinPriceInfo) {
+                Toast.makeText(this@CoinPriceListActivity, coinPriceInfo.fromSymbol, Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.rvCoinPriceList.adapter = adapter
+
 
         viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
-//        viewModel.priceList.observe(this, Observer {
-//            Log.d("TEST_OF_LOADING_DATA", "Success in activity: $it")
-//        })
-        viewModel.getDetailInfo("BTC").observe(this, Observer {
-            Log.d("TEST_OF_LOADING_DATA", "Success in activity: $it")
+        viewModel.priceList.observe(this, Observer {
+            adapter.coinInfoList = it
         })
 
-
     }
-
-
 }
