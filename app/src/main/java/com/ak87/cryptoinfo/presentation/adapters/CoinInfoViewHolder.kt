0c4ea4569ab06c83a573863976f9b71e0da2771e
Admin1 <a89086829981@gmail.com>
2023-06-06 14:32:41 +0300

    refactoring4

---
+++ app/src/main/java/com/ak87/cryptoinfo/presentation/adapters/CoinInfoViewHolder.kt
@@ -0,0 +1,9 @@
+package com.ak87.cryptoinfo.presentation.adapters
+
+import androidx.recyclerview.widget.RecyclerView
+import com.ak87.cryptoinfo.databinding.ItemCoinInfoBinding
+
+
+class CoinInfoViewHolder(
+    val binding: ItemCoinInfoBinding
+) : RecyclerView.ViewHolder(binding.root)
