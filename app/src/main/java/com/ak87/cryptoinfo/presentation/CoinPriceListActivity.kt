b24758db931861649188c23dedcdbb426a6a2ce5
Admin1 <a89086829981@gmail.com>
2023-06-06 16:42:53 +0300

    Add diff utils

--- app/src/main/java/com/ak87/cryptoinfo/presentation/CoinPriceListActivity.kt
+++ app/src/main/java/com/ak87/cryptoinfo/presentation/CoinPriceListActivity.kt
@@ -36,11 +36,12 @@ class CoinPriceListActivity : AppCompatActivity() {
             }
         }
         binding.rvCoinPriceList.adapter = adapter
+        binding.rvCoinPriceList.itemAnimator = null
 
 
         viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
         viewModel.coinInfoList.observe(this, Observer {
-            adapter.coinInfoList = it
+            adapter.submitList(it)
         })
 
     }
--- app/src/main/java/com/ak87/cryptoinfo/presentation/adapters/CoinInfoAdapter.kt
+++ app/src/main/java/com/ak87/cryptoinfo/presentation/adapters/CoinInfoAdapter.kt
@@ -3,20 +3,16 @@ package com.ak87.cryptoinfo.presentation.adapters
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.ak87.cryptoinfo.R
 import com.ak87.cryptoinfo.databinding.ItemCoinInfoBinding
 import com.ak87.cryptoinfo.domain.CoinInfo
 import com.squareup.picasso.Picasso
 
-class CoinInfoAdapter(private val context: Context) :
-    RecyclerView.Adapter<CoinInfoViewHolder>() {
-
-    var coinInfoList: List<CoinInfo> = listOf()
-    set(value) {
-        field = value
-        notifyDataSetChanged()
-    }
+class CoinInfoAdapter(
+    private val context: Context
+    ) : ListAdapter<CoinInfo, CoinInfoViewHolder>(CoinInfoDiffCallback) {
 
     var onCoinClickListener: OnCoinClickListener? = null
 
@@ -27,10 +23,10 @@ class CoinInfoAdapter(private val context: Context) :
         return CoinInfoViewHolder(binding)
     }
 
-    override fun getItemCount() = coinInfoList.size
+
 
     override fun onBindViewHolder(holder: CoinInfoViewHolder, position: Int) {
-        val coin = coinInfoList.get(position)
+        val coin = getItem(position)
 
         with(holder.binding) {
             with(coin) {
