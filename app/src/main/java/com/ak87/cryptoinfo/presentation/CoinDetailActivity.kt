aceeba4610d71f672fbb2dc15362f3b835715f8c
Admin1 <a89086829981@gmail.com>
2023-06-07 10:28:51 +0300

    Use CoinDetailFragment in CoinDetailActivity.kt

--- app/src/main/java/com/ak87/cryptoinfo/presentation/CoinDetailActivity.kt
+++ app/src/main/java/com/ak87/cryptoinfo/presentation/CoinDetailActivity.kt
@@ -4,14 +4,13 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.ViewModelProvider
+import com.ak87.cryptoinfo.R
 import com.ak87.cryptoinfo.databinding.ActivityCoinDetailBinding
-import com.squareup.picasso.Picasso
+import com.ak87.cryptoinfo.presentation.adapters.CoinDetailFragment
 
 class CoinDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCoinDetailBinding
-    private lateinit var viewModel: CoinViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,20 +20,11 @@ class CoinDetailActivity : AppCompatActivity() {
             return
         }
         val fromSymbol = intent.getStringExtra(EXTRA_FROM_SYMBOL) ?: EMPTY_SYMBOL
-        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
-        fromSymbol?.let {
-            viewModel.getDetailInfo(it).observe(this) {
-                with(binding) {
-                    tvPrice.text = it.price
-                    tvMinPrice.text = it.lowDay
-                    tvMaxPrice.text = it.highDay
-                    tvLastMarket.text = it.lastMarket
-                    tvLastUpdate.text = it.lastUpdate
-                    tvFromSymbol.text = it.fromSymbol
-                    tvToSymbol.text = it.toSymbol
-                    Picasso.get().load(it.imageUrl).into(ivLogoCoin)
-                }
-            }
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, CoinDetailFragment.newInstance(fromSymbol))
+                .commit()
         }
     }
 
